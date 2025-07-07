3be4fbd9475d52763373ee700290c5639551a41f
subbu <subbunandyala48@gmail.com>
2025-07-07 19:02:56 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components 
-    source   = "../../terraform-aws-roboshop"
+    source   = "git::https://github.com/subbu20n/terraform-aws-roboshop.git?ref=main"
     component = each.key 
     rule_priority = each.value.rul_priority 
 }
