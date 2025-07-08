183318d91421bc20a1bf5448cc76d7f2a97b2687
subbu <subbunandyala48@gmail.com>
2025-07-08 15:24:37 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,12 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro" 
   vpc_security_group_ids = [local.bastion_sg_id] 
   subnet_id = local.public_subnet_id 
+ 
+  #need more for terraform 
+  root_block_device {
+    volume_size = 50 
+    volume_type = "gp3" # or gp3, depending on your performance 
+  }
 
   tags = merge(
     local.common_tags,
