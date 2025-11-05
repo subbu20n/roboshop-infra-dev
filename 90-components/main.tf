66750752c40001da65bd7e1bbaa14c869a7a132f
subbu <subbunandyala48@gmail.com>
2025-11-05 17:38:46 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -3,5 +3,5 @@ module "component" {
     #source = "../../terraform-aws-roboshop" 
     source = "git::https://github.com/subbu20n/terraform-aws-roboshop.git?ref=main"
     component = each.key 
-    rule_priority = each.value.rule-priority 
-}
+    rule_priority = each.value.rule_priority 
+} 
